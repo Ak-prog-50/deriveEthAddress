38ecb7881466ca2e48dbcb5e519706547eb6054d
akalanka <akalankapathirage22@gmail.com>
2024-09-10 12:11:16 +0530

    deriveEthAddress2 var changes and removed 0x04 from uncompressed buffer since no slicing

--- deriveEthAddress2.js
+++ deriveEthAddress2.js
@@ -1,32 +1,51 @@
 // crbn backend early
-import { keccak256 } from "ethereumjs-util";
-import { Buffer } from "buffer";
+const keccak256 = require("ethereumjs-util").keccak256;
+const Buffer = require("buffer").Buffer;
 
-export const generatePublicKey = (curve, x, y) => {
+const givenEthAddress = "0x627306090abab3a6e1400e9345bc60c78a8bef57";
+
+const generatePublicKey = (curve, x, y) => {
   // Convert base64-encoded x and y to buffers
-  const xBuffer = Buffer.from(x, "base64");
-  const yBuffer = Buffer.from(y, "base64");
+  const xBuffer = Buffer.from(x, "hex");
+  const yBuffer = Buffer.from(y, "hex");
 
   // Concatenate x and y buffers to form the uncompressed public key
-  const publicKey = Buffer.concat([Buffer.from([0x04]), xBuffer, yBuffer]);
+  const uncompressedPubKeyBuffer = Buffer.concat([
+    // Buffer.from([0x04]),
+    xBuffer,
+    yBuffer,
+  ]);
 
   // Hash the public key using keccak-256
-  const addressBuffer = keccak256(publicKey);
+  const pubKeyHash = keccak256(uncompressedPubKeyBuffer);
 
   // Take the last 20 bytes of the hash to get the Ethereum address
-  const address = addressBuffer.slice(-20);
+  const walletAddress = pubKeyHash.slice(-20);
 
   // Convert the address to a hexadecimal string
-  const addressHex = `0x${address.toString("hex")}`;
+  const prefixedWalletAddressHex = `0x${walletAddress.toString("hex")}`;
 
-  console.log("Ethereum Address:", addressHex);
+  console.log("Ethereum Address:", prefixedWalletAddressHex);
+  console.log(
+    "Is the Ethereum Address correct?",
+    prefixedWalletAddressHex === givenEthAddress
+  );
 
   const walletData = {
     curve,
     x,
     y,
-    address: addressHex,
+    address: prefixedWalletAddressHex,
   };
 
   return walletData;
 };
+
+// x and y coordinates are from the public key in deriveEthAddress.js ( givenEthAddress )
+const walletData = generatePublicKey(
+  "secp256k1",
+  "af80b90d25145da28c583359beb47b21796b2fe1a23c1511e443e7a64dfdb27d",
+  "7434c380f0aa4c500e220aa1a9d068514b1ff4d5019e624e7ba1efe82b340a59"
+);
+
+console.log(walletData);
